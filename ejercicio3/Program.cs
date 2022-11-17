adb75c70a49642c697c169df6d1c2fb4f730f069
0Pasternak <107433719+0Pasternak@users.noreply.github.com>
2022-11-17 02:08:09 +0100

    Add files via upload

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ejercicio3{
+    class ejercicio3{
+        public static void Main(String [] arg){
+            Persona persona = new Persona();
+            persona.nombre = "Gabriel";
+            persona.edad = 24;
+            persona.telefono = 640274931;
+
+            System.Console.WriteLine($"{persona.nombre} {persona.edad} {persona.telefono}");
+        }
+    }
+    class Persona{
+        private int _edad;
+        private string _nombre;
+        private int _telefono;
+
+        public Persona(){
+            _nombre = "";
+            _edad = 0;
+            _telefono = 0;
+        }
+        public Persona(string nombre, int edad, int telefono){
+            this._nombre = nombre;
+            this._edad = edad;
+            this._telefono = telefono;
+        }
+
+        public int edad {get  => _edad; set => _edad = value;}
+        public string nombre {get  => _nombre; set => _nombre = value;}
+        public int telefono {get  => _telefono; set => _telefono = value;}
+
+    }
+}
