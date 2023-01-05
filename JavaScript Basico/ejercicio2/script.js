8ba541677f775ae9abaa5bcbd9dd4d0820b6f9d5
0Pasternak <107433719+0Pasternak@users.noreply.github.com>
2023-01-05 02:00:32 +0100

    Add files via upload

---
+++ JavaScript Basico/ejercicio2/script.js
@@ -0,0 +1,13 @@
+let nombre = "Alex";
+let edad = 20;
+let desarrollador = true;
+let nacimiento = new Date("november 22 2002")
+let libro = {
+    nombre: "1984",
+    autor: "george orwell",
+    fecha: 1949,
+    link: "https://www.amazon.es/1984-Contempor%C3%A1nea-CONTEMPORANEA-George-Orwell/dp/8499890946/ref=asc_df_8499890946/?tag=googshopes-21&linkCode=df0&hvadid=195225763566&hvpos=&hvnetw=g&hvrand=4861537950315669001&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005489&hvtargid=pla-138218692395&psc=1"
+}
+
+
+let lista = [nombre, edad, desarrollador, nacimiento, libro];
